8d307ff33041f7d523ed421d2247d431017ed043
Brian <87432830+phoenixArise@users.noreply.github.com>
2023-03-13 21:33:12 -0400

    Initial Commit

---
+++ Safe Surfer/Safe_SurferApp.swift
@@ -0,0 +1,17 @@
+//
+//  Safe_SurferApp.swift
+//  Safe Surfer
+//
+//  Created by Brian Seo on 2023-03-13.
+//
+
+import SwiftUI
+
+@main
+struct Safe_SurferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
